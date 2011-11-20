b4ec3d3652f9ae132146223925556602781bff84
Tamas Eppel <tamas.eppel@gmail.com>
2011-11-20 13:37:52 +0100

    parsing joda time as well
    * adding a dependency on joda time, so that is possible to parse that
    * shortcutting on null values

--- src/main/java/com/typemapper/core/fieldMapper/DateFieldMapper.java
+++ src/main/java/com/typemapper/core/fieldMapper/DateFieldMapper.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 
 public class DateFieldMapper implements FieldMapper {
 
@@ -15,6 +16,9 @@ public class DateFieldMapper implements FieldMapper {
 
 	@Override
 	public Object mapField(String string, Class clazz) {
+		if (string == null || string.isEmpty()) {
+			return null;
+		}
 		Date result = null;
 		for (final String format : FORMATS) {
 			try {
@@ -28,7 +32,9 @@ public class DateFieldMapper implements FieldMapper {
 		if (result == null) {
 			LOG.error("Could not parse date: " + string);
 		}
-		if (clazz != null && clazz.equals(java.sql.Date.class) && result != null) {
+		if (clazz != null && clazz.equals(DateTime.class) && result != null) {
+			return new DateTime(result.getTime());
+		} else if (clazz != null && clazz.equals(java.sql.Date.class) && result != null) {
 			return new java.sql.Date(result.getTime());
 		} else {
 			return result;
--- src/test/java/com/typemapper/core/fieldMapper/DateFieldMapperValuesTest.java
+++ src/test/java/com/typemapper/core/fieldMapper/DateFieldMapperValuesTest.java
@@ -1,12 +1,15 @@
 package com.typemapper.core.fieldMapper;
 
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class DateFieldMapperValuesTest {
@@ -31,20 +34,36 @@ public class DateFieldMapperValuesTest {
 	public void nullClassDoesNotBorkMethod() throws Exception {
 		Object result = MAPPER.mapField(DATE_STRING, null);
 		assertNotNull(result);
-		assertEquals(Date.class, result.getClass());
+		assertThat("util date", result, instanceOf(Date.class));
 	}
 
 	@Test
 	public void wrongClassDoesNotBorkMethod() throws Exception {
 		Object result = MAPPER.mapField(DATE_STRING, String.class);
 		assertNotNull(result);
-		assertEquals(Date.class, result.getClass());
+		assertThat("util date", result, instanceOf(Date.class));
 	}
 
 	@Test
 	public void sqlDateClassReturnsCorrectObject() throws Exception {
 		Object result = MAPPER.mapField(DATE_STRING, java.sql.Date.class);
 		assertNotNull(result);
-		assertEquals(java.sql.Date.class, result.getClass());
+		assertThat("sql date", result, instanceOf(java.sql.Date.class));
+	}
+	
+	@Test
+	public void returnsNullOnNullOrEmpty() throws Exception {
+		Object result = MAPPER.mapField(null, Date.class);
+		assertNull("on null", result);
+
+		result = MAPPER.mapField("", Date.class);
+		assertNull("on empty", result);
+	}
+
+	@Test
+	public void jodaTimeDateClassReturnsCorrectObject() throws Exception {
+		Object result = MAPPER.mapField(DATE_STRING, DateTime.class);
+		assertNotNull(result);
+		assertThat("joda time", result, instanceOf(DateTime.class));
 	}
 }
