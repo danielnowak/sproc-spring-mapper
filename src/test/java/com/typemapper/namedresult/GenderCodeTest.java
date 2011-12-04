5a380a1dbb6eb435d4f0447b553dccd18fe8096d
Jan Gorman <gorman.jan@gmail.com>
2011-12-04 15:47:46 +0100

    Parameterized enum mapping test

--- src/test/java/com/typemapper/namedresult/GenderCodeTest.java
+++ src/test/java/com/typemapper/namedresult/GenderCodeTest.java
@@ -5,8 +5,13 @@ import static org.junit.Assert.assertNotNull;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.typemapper.AbstractTest;
 import com.typemapper.core.TypeMapper;
@@ -14,18 +19,37 @@ import com.typemapper.core.TypeMapperFactory;
 import com.typemapper.namedresult.results.ClassWithGenderCode;
 import com.typemapper.namedresult.results.GenderCode;
 
+@RunWith(Parameterized.class)
 public class GenderCodeTest extends AbstractTest {
+    
+    private final String dbCode;
+    private final GenderCode genderCode;
+    
+    public GenderCodeTest(final String dbCode, final GenderCode genderCode) {
+        this.dbCode = dbCode;
+        this.genderCode = genderCode;
+    }
+    
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+        final Object[][] data = new Object[][] {
+                {"femme", GenderCode.FEMALE},
+                {"homme", GenderCode.MALE}
+        };
+        return Arrays.asList(data);
+    }
     
     @Test
     public void enumTransformation() throws Exception {
-        final PreparedStatement stmt = connection.prepareStatement("SELECT 'femme' AS male_female;");
+        final PreparedStatement stmt = connection.prepareStatement(String.format("SELECT '%s' AS male_female;",
+                dbCode));
         final ResultSet result = stmt.executeQuery();
         final TypeMapper<ClassWithGenderCode> mapper = TypeMapperFactory.createTypeMapper(ClassWithGenderCode.class);
         int i = 0;
         while(result.next()) {
             final ClassWithGenderCode classWithGenderCode = mapper.mapRow(result, i++);
             assertNotNull(classWithGenderCode);
-            assertEquals(GenderCode.FEMALE, classWithGenderCode.getGenderCode());
+            assertEquals(genderCode, classWithGenderCode.getGenderCode());
         }
     }
 
