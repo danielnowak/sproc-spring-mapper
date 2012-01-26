21559c92ebb4762eaae1fbab47e343c328b95777
Henning Jacobs <henning@jacobs1.de>
2012-01-26 09:57:09 +0100

    fix ParseUtils.postgresROW2StringList() (returned too few fields for rows starting with too null values)

--- src/main/java/com/typemapper/parser/postgres/ParseUtils.java
+++ src/main/java/com/typemapper/parser/postgres/ParseUtils.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.typemapper.parser.postgres;
 
@@ -13,172 +13,192 @@ import com.typemapper.parser.exception.ParserException;
 import com.typemapper.parser.exception.RowParserException;
 
 /**
- * @author valgog
- * 
+ * @author  valgog
  */
 public class ParseUtils {
 
-	public static final List<String> postgresArray2StringList(String value)
-			throws ArrayParserException {
-		return postgresArray2StringList(value, 16);
-	}
+    public static final List<String> postgresArray2StringList(final String value) throws ArrayParserException {
+        return postgresArray2StringList(value, 16);
+    }
 
-	public static final List<String> postgresArray2StringList(String value,
-			int appendStringSize) throws ArrayParserException {
-		if (!(value.startsWith("{") && value.endsWith("}")))
-			throw new ArrayParserException(
-					String
-							.format(
-									"postgresArray2StringList() ARRAY must begin with '{' and ends with '}': %s",
-									value));
-		if (value.length() == 2) {
-			return Collections.emptyList();
-		}
-		// This is a simple copy-paste from the ROW processing code, and
-		// strictly speaking is not quite correct for PostgreSQL ARRAYs
-		List<String> result = new ArrayList<String>();
+    public static final List<String> postgresArray2StringList(final String value, final int appendStringSize)
+        throws ArrayParserException {
+        if (!(value.startsWith("{") && value.endsWith("}"))) {
+            throw new ArrayParserException(String.format(
+                    "postgresArray2StringList() ARRAY must begin with '{' and ends with '}': %s", value));
+        }
 
-		char[] c = value.toCharArray();
+        if (value.length() == 2) {
+            return Collections.emptyList();
+        }
 
-		StringBuilder element = new StringBuilder(appendStringSize);
-		// this processor will fail if value has spaces between ',' and '"' or
-		// ')'
-		int i = 1;
-		while (c[i] != '}') {
-			if (c[i] == ',') {
-				char nextChar = c[i + 1];
-				if (nextChar == ',' || nextChar == '}') {
-					// we have an empty position, that is we have a NULL value
-					result.add(null);
-				}
-				i++;
-			} else if (c[i] == '\"') {
-				i++;
-				boolean insideQuote = true;
-				while (insideQuote) {
-					char nextChar = c[i + 1];
-					if (c[i] == '\"') {
-						if (nextChar == ',' || nextChar == '}') {
-							result.add(element.toString());
-							element = new StringBuilder(appendStringSize);
-							insideQuote = false;
-						} else if (nextChar == '\"') {
-							i++;
-							element.append(c[i]);
-						} else {
-							throw new ArrayParserException(
-									"postgresArray2StringList() char after \" is not valid");
-						}
-					} else if (c[i] == '\\') {
-						if (nextChar == '\\' || nextChar == '\"') {
-							i++;
-							element.append(c[i]);
-						} else {
-							throw new ArrayParserException(
-									"postgresArray2StringList() char after \\ is not valid");
-						}
-					} else {
-						element.append(c[i]);
-					}
-					i++;
-				}
-			} else {
-				while (!(c[i] == ',' || c[i] == '}')) {
-					element.append(c[i]);
-					i++;
-				}
-				// if the element was not quoted and was empty, it is supposed
-				// to be NULL
-				result.add(element.length() > 0 ? element.toString() : null);
-				element = new StringBuilder(appendStringSize);
-			}
-		}
-		return result;
-	}
+        // This is a simple copy-paste from the ROW processing code, and
+        // strictly speaking is not quite correct for PostgreSQL ARRAYs
+        List<String> result = new ArrayList<String>();
 
-	public static final List<String> postgresROW2StringList(String value)
-			throws RowParserException {
-		return postgresROW2StringList(value, 16);
-	}
+        char[] c = value.toCharArray();
 
-	public static final List<String> postgresROW2StringList(String value,
-			int appendStringSize) throws RowParserException {
-		if (!(value.startsWith("(") && value.endsWith(")")))
-			throw new RowParserException(
-					"postgresROW2StringList() ROW must begin with '(' and ends with ')': "
-							+ value);
+        StringBuilder element = new StringBuilder(appendStringSize);
 
-		List<String> result = new ArrayList<String>();
+        // this processor will fail if value has spaces between ',' and '"' or
+        // ')'
+        int i = 1;
+        while (c[i] != '}') {
+            if (c[i] == ',') {
+                char nextChar = c[i + 1];
+                if (nextChar == ',' || nextChar == '}') {
 
-		char[] c = value.toCharArray();
+                    // we have an empty position, that is we have a NULL value
+                    result.add(null);
+                }
 
-		StringBuilder element = new StringBuilder(appendStringSize);
-		// this processor will fail if value has spaces between ',' and '"' or
-		// ')'
-		int i = 1;
-		while (c[i] != ')') {
-			if (c[i] == ',') {
-				char nextChar = c[i + 1];
-				if (nextChar == ',' || nextChar == ')' || c[i - 1] == '(') {
-					// we have an empty position, that is we have a NULL value
-					result.add(null);
-				}
-				i++;
-			} else if (c[i] == '\"') {
-				i++;
-				boolean insideQuote = true;
-				while (insideQuote) {
-					char nextChar = c[i + 1];
-					if (c[i] == '\"') {
-						if (nextChar == ',' || nextChar == ')') {
-							result.add(element.toString());
-							element = new StringBuilder(appendStringSize);
-							insideQuote = false;
-						} else if (nextChar == '\"') {
-							i++;
-							element.append(c[i]);
-						} else {
-							throw new RowParserException(
-									"postgresROW2StringList() char after \" is not valid");
-						}
-					} else if (c[i] == '\\') {
-						if (nextChar == '\\' || nextChar == '\"') {
-							i++;
-							element.append(c[i]);
-						} else {
-							throw new RowParserException(
-									"postgresROW2StringList() char after \\ is not valid");
-						}
-					} else {
-						element.append(c[i]);
-					}
-					i++;
-				}
-			} else {
-				while (!(c[i] == ',' || c[i] == ')')) {
-					element.append(c[i]);
-					i++;
-				}
-				// if the element was not quoted and was empty, it is supposed
-				// to be NULL
-				result.add(element.length() > 0 ? element.toString() : null);
-				element = new StringBuilder(appendStringSize);
-			}
-		}
-		return result;
-	}
+                i++;
+            } else if (c[i] == '\"') {
+                i++;
 
-	private static final String[] trueList = { "t", "true", "1" };
-	private static final String[] falseList = { "f", "false", "0" };
-	
-	public static Boolean getBoolean(String s) throws ParserException {
-		if ( s == null ) return null;
-		String b = s.trim().toLowerCase(Locale.US).intern();
-		for ( int i = 0; i < 3; i ++ ) {
-			if ( b.equals(trueList[i]) ) return Boolean.TRUE;
-			if ( b.equals(falseList[i]) ) return Boolean.FALSE;
-		}
-		throw new ParserException("Cannot convert sting [" + s + "] to Boolean");
-	}
-	
+                boolean insideQuote = true;
+                while (insideQuote) {
+                    char nextChar = c[i + 1];
+                    if (c[i] == '\"') {
+                        if (nextChar == ',' || nextChar == '}') {
+                            result.add(element.toString());
+                            element = new StringBuilder(appendStringSize);
+                            insideQuote = false;
+                        } else if (nextChar == '\"') {
+                            i++;
+                            element.append(c[i]);
+                        } else {
+                            throw new ArrayParserException("postgresArray2StringList() char after \" is not valid");
+                        }
+                    } else if (c[i] == '\\') {
+                        if (nextChar == '\\' || nextChar == '\"') {
+                            i++;
+                            element.append(c[i]);
+                        } else {
+                            throw new ArrayParserException("postgresArray2StringList() char after \\ is not valid");
+                        }
+                    } else {
+                        element.append(c[i]);
+                    }
+
+                    i++;
+                }
+            } else {
+                while (!(c[i] == ',' || c[i] == '}')) {
+                    element.append(c[i]);
+                    i++;
+                }
+
+                // if the element was not quoted and was empty, it is supposed
+                // to be NULL
+                result.add(element.length() > 0 ? element.toString() : null);
+                element = new StringBuilder(appendStringSize);
+            }
+        }
+
+        return result;
+    }
+
+    public static final List<String> postgresROW2StringList(final String value) throws RowParserException {
+        return postgresROW2StringList(value, 16);
+    }
+
+    public static final List<String> postgresROW2StringList(final String value, final int appendStringSize)
+        throws RowParserException {
+        if (!(value.startsWith("(") && value.endsWith(")"))) {
+            throw new RowParserException("postgresROW2StringList() ROW must begin with '(' and ends with ')': "
+                    + value);
+        }
+
+        List<String> result = new ArrayList<String>();
+
+        char[] c = value.toCharArray();
+
+        StringBuilder element = new StringBuilder(appendStringSize);
+
+        // this processor will fail if value has spaces between ',' and '"' or
+        // ')'
+        int i = 1;
+        while (c[i] != ')') {
+            if (c[i] == ',') {
+                char nextChar = c[i + 1];
+                if (c[i - 1] == '(') {
+
+                    // we have an empty first position, that is we have a NULL value
+                    result.add(null);
+                }
+
+                if (nextChar == ',' || nextChar == ')') {
+                    result.add(null);
+                }
+
+                i++;
+            } else if (c[i] == '\"') {
+                i++;
+
+                boolean insideQuote = true;
+                while (insideQuote) {
+                    char nextChar = c[i + 1];
+                    if (c[i] == '\"') {
+                        if (nextChar == ',' || nextChar == ')') {
+                            result.add(element.toString());
+                            element = new StringBuilder(appendStringSize);
+                            insideQuote = false;
+                        } else if (nextChar == '\"') {
+                            i++;
+                            element.append(c[i]);
+                        } else {
+                            throw new RowParserException("postgresROW2StringList() char after \" is not valid");
+                        }
+                    } else if (c[i] == '\\') {
+                        if (nextChar == '\\' || nextChar == '\"') {
+                            i++;
+                            element.append(c[i]);
+                        } else {
+                            throw new RowParserException("postgresROW2StringList() char after \\ is not valid");
+                        }
+                    } else {
+                        element.append(c[i]);
+                    }
+
+                    i++;
+                }
+            } else {
+                while (!(c[i] == ',' || c[i] == ')')) {
+                    element.append(c[i]);
+                    i++;
+                }
+
+                // if the element was not quoted and was empty, it is supposed
+                // to be NULL
+                result.add(element.length() > 0 ? element.toString() : null);
+                element = new StringBuilder(appendStringSize);
+            }
+        }
+
+        return result;
+    }
+
+    private static final String[] trueList = {"t", "true", "1"};
+    private static final String[] falseList = {"f", "false", "0"};
+
+    public static Boolean getBoolean(final String s) throws ParserException {
+        if (s == null) {
+            return null;
+        }
+
+        String b = s.trim().toLowerCase(Locale.US).intern();
+        for (int i = 0; i < 3; i++) {
+            if (b.equals(trueList[i])) {
+                return Boolean.TRUE;
+            }
+
+            if (b.equals(falseList[i])) {
+                return Boolean.FALSE;
+            }
+        }
+
+        throw new ParserException("Cannot convert sting [" + s + "] to Boolean");
+    }
+
 }
--- src/test/java/com/typemapper/parser/postgres/ParseUtilsTest.java
+++ src/test/java/com/typemapper/parser/postgres/ParseUtilsTest.java
@@ -2,7 +2,6 @@ package com.typemapper.parser.postgres;
 
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.typemapper.parser.exception.RowParserException;
@@ -15,10 +14,42 @@ import junit.framework.Assert;
 public class ParseUtilsTest {
 
     @Test
-    @Ignore("TODO: This test fails because postgresROW2StringList is BUGGY!")
+    public void testPostgresRow2StringListSimple() throws RowParserException {
+
+        List<String> fieldValues;
+        fieldValues = ParseUtils.postgresROW2StringList("(a,b)");
+        Assert.assertEquals(2, fieldValues.size());
+        Assert.assertEquals("a", fieldValues.get(0));
+        Assert.assertEquals("b", fieldValues.get(1));
+
+        fieldValues = ParseUtils.postgresROW2StringList("(a,)");
+        Assert.assertEquals(2, fieldValues.size());
+        Assert.assertEquals("a", fieldValues.get(0));
+        Assert.assertNull(fieldValues.get(1));
+
+        fieldValues = ParseUtils.postgresROW2StringList("(,b)");
+        Assert.assertEquals(2, fieldValues.size());
+        Assert.assertNull(fieldValues.get(0));
+        Assert.assertEquals("b", fieldValues.get(1));
+
+        fieldValues = ParseUtils.postgresROW2StringList("(,)");
+        Assert.assertEquals(2, fieldValues.size());
+        Assert.assertNull(fieldValues.get(0));
+        Assert.assertNull(fieldValues.get(1));
+
+        fieldValues = ParseUtils.postgresROW2StringList("(a,,,b)");
+        Assert.assertEquals(4, fieldValues.size());
+        Assert.assertEquals("a", fieldValues.get(0));
+        Assert.assertNull(fieldValues.get(1));
+        Assert.assertNull(fieldValues.get(2));
+        Assert.assertEquals("b", fieldValues.get(3));
+    }
+
+    @Test
     public void testPostgresRow2StringList() throws RowParserException {
 
         List<String> fieldValues;
+
         fieldValues = ParseUtils.postgresROW2StringList("(,,0,1)");
         Assert.assertEquals(4, fieldValues.size());
         Assert.assertNull(fieldValues.get(0));
