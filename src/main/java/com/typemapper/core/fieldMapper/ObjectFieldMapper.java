60fdddf807a57ae020f6b4158282334b23b126bc
Henning Jacobs <henning@jacobs1.de>
2012-02-22 20:53:40 +0100

    catch mapping error and log

--- src/main/java/com/typemapper/core/fieldMapper/ObjectFieldMapper.java
+++ src/main/java/com/typemapper/core/fieldMapper/ObjectFieldMapper.java
@@ -1,6 +1,7 @@
 package com.typemapper.core.fieldMapper;
 
 import java.lang.reflect.InvocationTargetException;
+
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -10,35 +11,45 @@ import com.typemapper.core.result.ArrayResultNode;
 import com.typemapper.core.result.DbResultNode;
 import com.typemapper.core.result.DbResultNodeType;
 import com.typemapper.core.result.ObjectResultNode;
+
 import com.typemapper.exception.NotsupportedTypeException;
 
 public class ObjectFieldMapper {
-	
-	private static final Logger LOG = Logger.getLogger(ObjectFieldMapper.class);
-	
-	public final static Object mapField(@SuppressWarnings("rawtypes") Class clazz, ObjectResultNode node) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NotsupportedTypeException {
-		if (node.getChildren() == null) {
-			return null;
-		}
-		List<Mapping> mappings = Mapping.getMappingsForClass(clazz);
-		Object result = clazz.newInstance();
-		
-		for (Mapping mapping: mappings) {
-			DbResultNode currentNode = node.getChildByName(mapping.getName());
-			if (currentNode == null) {
-				LOG.warn("Could not find value of mapping: " + mapping.getName());
-				continue;
-			}
-			if (DbResultNodeType.SIMPLE.equals(currentNode.getNodeType())) {
-				mapping.map(result, mapping.getFieldMapper().mapField(currentNode.getValue(), mapping.getFieldClass()));
-			} else if (DbResultNodeType.OBJECT.equals(currentNode.getNodeType())) {
-				mapping.map(result, mapField(mapping.getFieldClass(), (ObjectResultNode) currentNode));
-			} else if (DbResultNodeType.ARRAY.equals(currentNode.getNodeType())) {
-				mapping.map(result, ArrayFieldMapper.mapField(mapping.getField(), (ArrayResultNode) currentNode));
-			}
-		}
-		
-		return result;
-	}
+
+    private static final Logger LOG = Logger.getLogger(ObjectFieldMapper.class);
+
+    public static final Object mapField(@SuppressWarnings("rawtypes") final Class clazz, final ObjectResultNode node)
+        throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+            NotsupportedTypeException {
+        if (node.getChildren() == null) {
+            return null;
+        }
+
+        List<Mapping> mappings = Mapping.getMappingsForClass(clazz);
+        Object result = clazz.newInstance();
+
+        for (Mapping mapping : mappings) {
+            DbResultNode currentNode = node.getChildByName(mapping.getName());
+            if (currentNode == null) {
+                LOG.warn("Could not find value of mapping: " + mapping.getName());
+                continue;
+            }
+
+            try {
+                if (DbResultNodeType.SIMPLE.equals(currentNode.getNodeType())) {
+                    mapping.map(result,
+                        mapping.getFieldMapper().mapField(currentNode.getValue(), mapping.getFieldClass()));
+                } else if (DbResultNodeType.OBJECT.equals(currentNode.getNodeType())) {
+                    mapping.map(result, mapField(mapping.getFieldClass(), (ObjectResultNode) currentNode));
+                } else if (DbResultNodeType.ARRAY.equals(currentNode.getNodeType())) {
+                    mapping.map(result, ArrayFieldMapper.mapField(mapping.getField(), (ArrayResultNode) currentNode));
+                }
+            } catch (Exception e) {
+                LOG.error("Failed to map property " + mapping.getName() + " of class " + clazz.getSimpleName(), e);
+            }
+        }
+
+        return result;
+    }
 
 }
